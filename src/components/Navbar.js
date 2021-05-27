6185f5f64e946aaea6ae939c01fbc6a4b0f7781d
hackeziah <lamadridkevinpaul@gmail.com>
2021-05-28 00:48:53 +0800

    adding usecontext

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,46 @@
 import React from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
+import { AuthContext } from "../contexts/AuthContext";
 
 class Navbar extends React.Component {
-  static contextType = ThemeContext;
+  // static contextType = ThemeContext;
 
   render() {
     return (
-      <ThemeContext.Consumer>
-        {(context) => {
-          const { isDarkTheme, darkTheme, lightTheme } = context;
-          const theme = isDarkTheme ? darkTheme : lightTheme;
+      <AuthContext.Consumer>
+        {(authContext) => {
           return (
-            <nav
-              style={{
-                background: theme.background,
-                color: theme.text,
-                height: "120px",
+            <ThemeContext.Consumer>
+              {(themeContext) => {
+                const { isDarkTheme, darkTheme, lightTheme } = themeContext;
+
+                const { isLoggedIn, changeAuthStatus } = authContext;
+                const theme = isDarkTheme ? darkTheme : lightTheme;
+
+                return (
+                  <nav
+                    style={{
+                      background: theme.background,
+                      color: theme.text,
+                      height: "120px",
+                    }}
+                  >
+                    <p onClick={changeAuthStatus} style={{ textAlign: "center" }}>
+                      {isLoggedIn ? "Login" : "Logout"} 
+                    </p>
+                    <h2 style={{ textAlign: "center" }}>Atmos Academy</h2>
+                    <div className="ui three buttons">
+                      <button className="ui button">Overview</button>
+                      <button className="ui button">Contact</button>
+                      <button className="ui button">Support</button>
+                    </div>
+                  </nav>
+                );
               }}
-            >
-              <h2 style={{ textAlign: "center" }}>Atmos Academy</h2>
-              <div className="ui three buttons">
-                <button className="ui button">Overview</button>
-                <button className="ui button">Contact</button>
-                <button className="ui button">Support</button>
-              </div>
-            </nav>
+            </ThemeContext.Consumer>
           );
         }}
-      </ThemeContext.Consumer>
+      </AuthContext.Consumer>
     );
   }
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,30 +1,52 @@
-import React from "react";
+import React, {useContext} from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
-class TodoList extends React.Component {
-  static contextType = ThemeContext;
-  render() {
-    const { isDarkTheme, lightTheme, darkTheme, changeTheme} = this.context;
-    const theme = isDarkTheme ? darkTheme : lightTheme;
+// class TodoList extends React.Component {
+//   static contextType = ThemeContext;
+//   render() {
+//     const { isDarkTheme, lightTheme, darkTheme, changeTheme} = this.context;
+//     const theme = isDarkTheme ? darkTheme : lightTheme;
 
-    return (
-      <div
-        style={{
-          background: theme.background,
-          color: theme.text,
-          height: "140px",
-          textAlign: "center",
-        }}
-      >
-        <p className="item"> Plan the family trip</p>
-        <p className="item"> Atmos Kaizen</p>
-        <p className="item"> Walking 6 am</p>
-        <button className='ui button primary' onClick={changeTheme}>
-        Change Theme
-        </button>
-      </div>
-    );
-  }
+//     return (
+//       <div
+//         style={{
+//           background: theme.background,
+//           color: theme.text,
+//           height: "140px",
+//           textAlign: "center",
+//         }}
+//       >
+//         <p className="item"> Plan the family trip</p>
+//         <p className="item"> Atmos Kaizen</p>
+//         <p className="item"> Walking 6 am</p>
+//         <button className='ui button primary' onClick={changeTheme}>
+//         Change Theme
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+
+const TodoList = () => {
+  const { isDarkTheme, lightTheme, darkTheme, changeTheme} = useContext(ThemeContext);
+  const theme = isDarkTheme ? darkTheme : lightTheme;
+
+  return (
+    <div
+    style={{
+      background: theme.background,
+      color: theme.text,
+      height: "140px",
+      textAlign: "center",
+    }}
+  >
+    <p className="item"> Plan the family trip</p>
+    <p className="item"> Atmos Kaizen</p>
+    <p className="item"> Walking 6 am</p>
+    <button className='ui button primary' onClick={changeTheme}>
+    Change Theme
+    </button>
+  </div>
+  );
 }
-
 export default TodoList;
